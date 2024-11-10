0f216800ae522311fe60071ddb83167bcd62c5b6
shivamgaur9977 <shivamgaur9977@gmail.com>
2024-11-11 00:03:21 +0530

    Commited

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  host: '0.0.0.0',
+    port: process.env.PORT || 5173,  // Use the PORT variable from Render
   plugins: [react()],
 })
